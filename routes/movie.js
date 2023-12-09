59ba012153624f9403a94b95a2319e9d3d52695e
danijuhaeni22 <danijuhaeni22@gmail.com>
2023-12-09 20:42:43 +0700

    add auth

--- routes/movie.js
+++ routes/movie.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const MovieController = require('../controller/movie_controller')
-const auth = require("../middleware/auth")
+const MovieController = require("../controller/movie_controller");
+const auth = require("../middleware/auth");
 
-router.get('/', MovieController.getAll)
-router.get('/:id',  MovieController.getOne)
-router.post('/', MovieController.create)
-router.put('/:id',   MovieController.update)
-router.delete('/:id',  MovieController.delete)
-
-
+router.get("/", auth, MovieController.getAll);
+router.get("/:id", auth, MovieController.getOne);
+router.post("/", auth, MovieController.create);
+router.put("/:id", auth, MovieController.update);
+router.delete("/:id", auth, MovieController.delete);
 
 module.exports = router;
